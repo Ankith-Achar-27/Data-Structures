9d42d8d86702b22896f0bf270339ba78984c1481
Ankith H S <ankith270600@gmail.com>
2026-07-02 20:32:12 +0530

    deleted test cases to simplify

--- Maths/Bit_Manipulation/OddOrEven.java
+++ Maths/Bit_Manipulation/OddOrEven.java
@@ -3,33 +3,18 @@ package Bit_Manipulation;
 // Check a number is odd or even using bit manipulation
 // Logic is: number & 1 == 1 then it is odd else even
 
-class TestOddEven{
-    int num;
 
-    public TestOddEven(int num) {
-        this.num = num;
-    }
-}
 
 public class OddOrEven {
     public static void main(String[] args) {
-        TestOddEven test1 = new TestOddEven(3);
-        TestOddEven test2 = new TestOddEven(0);
-        TestOddEven test3 = new TestOddEven(10);
-        TestOddEven test4 = new TestOddEven(17);
-
-        checknum(test1.num);
-        checknum(test2.num);
-        checknum(test3.num);
-        checknum(test4.num);
-
+        System.out.println(checknum(3));
+        System.out.println(checknum(0));
+        System.out.println(checknum(10));
+        System.out.println(checknum(17));
     }
-    static void checknum(int num){
-        if((num & 1) ==1){
-            System.out.println(num+" is odd number");
-        }
-        else{
-            System.out.println(num+" is even number");
-        }
+
+    static String checknum(int num){
+        return (num & 1) ==1 ? num + " is odd number"
+                : num + " is even number" ;
     }
 }
