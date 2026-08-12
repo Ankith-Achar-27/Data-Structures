53afc074aeb2bfd0b2e0945930981fc9afa920d2
Ankith H S <ankith270600@gmail.com>
2026-08-12 21:15:19 +0530

    Count number of zeroes

---
+++ Recursion/CountNoOfZeros.java
@@ -0,0 +1,25 @@
+public class CountNoOfZeros {
+    public static void main(String[] args) {
+        int n = 101001;
+        int ans = count(n);
+        System.out.println(ans);
+
+    }
+    static int count(int n){
+        if(n==0){
+            return 1;
+        }
+        return helper(n,0);
+    }
+
+    static int helper(int n, int c) {
+        if(n == 0){
+            return c;
+        }
+        int rem = n%10;
+        if(rem == 0){
+            return helper(n/10,c+1);
+        }
+        return helper(n/10,c);
+    }
+}
