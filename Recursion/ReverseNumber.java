952b77d7aa97d61b56b51eefd5c9420bae1dcc25
Ankith H S <ankith270600@gmail.com>
2026-08-12 21:13:52 +0530

    Reverse a Number

---
+++ Recursion/ReverseNumber.java
@@ -0,0 +1,18 @@
+public class ReverseNumber {
+
+    static int sum = 0;
+
+    static void reverse(int n){
+        if(n == 0){
+            return;
+        }
+        int rem = n%10;
+        sum = sum*10+rem;
+        reverse(n/10);
+    }
+    public static void main(String[] args) {
+        int n = 1234;
+        reverse(n);
+        System.out.println(sum);
+    }
+}
