24f36d855720559cd55aba6b100ca75cf6b769c8
Ankith H S <ankith270600@gmail.com>
2026-08-13 21:18:09 +0530

    Check Array is sorted or not

---
+++ Recursion/CheckArraySorted.java
@@ -0,0 +1,12 @@
+public class CheckArraySorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println(check(arr,0));
+    }
+    static boolean check(int[] arr,int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        return (arr[i]<arr[i+1])&&check(arr,i+1);
+    }
+}
