54064fd9fe36f4229e4b2b908308ddca1a0a3b09
Ankith H S <ankith270600@gmail.com>
2026-08-13 21:50:13 +0530

    updated all possible way to find

--- Recursion/LinearSearchRecursion.java
+++ Recursion/LinearSearchRecursion.java
@@ -1,22 +1,48 @@
+import java.util.ArrayList;
+
 public class LinearSearchRecursion {
     public static void main(String[] args) {
-        int[] arr = {1,43,12,5,67};
-        int target = 5;
-        int pos = linear(arr,target,0);
-        if(pos==-1){
-            System.out.println("Target is not found.");
-        }
-        else{
-            System.out.println("Target found at position: "+(pos+1));
-        }
+        int[] arr = {1,2,3,4,4,5};
+        int target = 4;
+        System.out.println(find(arr,target,0));
+        System.out.println(findIndex(arr,target,0));
+        System.out.println(findLastIndex(arr,target, arr.length-1));
+        findAllIndex(arr,target, 0);
+        System.out.println(list);
+
     }
-    static int linear(int[] arr,int tar,int index){
+    static boolean find(int[]arr,int tar,int index){
+        if(index == arr.length){
+            return false;
+        }
+        return arr[index] == tar || find(arr, tar, index+1);
+    }
+    static int findIndex(int[] arr, int tar, int index){
         if(index== arr.length){
             return -1;
         }
         if(arr[index] == tar){
             return index;
         }
-        return linear(arr,tar,index+1);
+        return findIndex(arr,tar,index+1);
+    }
+    static int findLastIndex(int[] arr, int tar, int index){
+        if(index== -1){
+            return -1;
+        }
+        if(arr[index] == tar){
+            return index;
+        }
+        return findIndex(arr,tar,index-1);
+    }
+    static ArrayList<Integer> list = new ArrayList<>();
+    static void findAllIndex(int[] arr,int tar,int index){
+        if(index== arr.length){
+            return;
+        }
+        if(arr[index] == tar){
+            list.add(index);
+        }
+        findAllIndex(arr, tar, index+1);
     }
 }
